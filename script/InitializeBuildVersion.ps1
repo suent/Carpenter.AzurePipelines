f7bc50b9c9efb1108d4578ab97bfd1e65f7c4cbe
Jeffrey Block <jeffrey.block@suent.net>
2022-01-17 11:09:51 -0700

    Use versionFile

--- script/InitializeBuildVersion.ps1
+++ script/InitializeBuildVersion.ps1
@@ -7,9 +7,25 @@
 
 [CmdletBinding()]
 param(
-
+	[string] $SourcesDirectory = $env:SOURCES_DIRECTORY,
+	[string] $VersionFile = $env:CARPENTER_VERSION_VERSIONFILE
 )
 
 $scriptName = Split-Path $PSCommandPath -Leaf
 
-Write-Host "$scriptName"
+. "$PSScriptRoot\include\Carpenter.AzurePipelines.Common.ps1"
+
+Write-ScriptHeader "$scriptName"
+
+$versionFilePath = "$SourcesDirectory\$VersionFile"
+If (-Not (Test-Path -Path $versionFilePath -PathType Leaf)) {
+	Write-Error "VERSION file does not exist at expected path. Path: $versionFilePath"
+} else {
+	Write-Verbose "Using version file: $versionFilePath"
+	$targetVersion = Get-Content -Path $versionFilePath
+	Write-Verbose "Base Version: $targetVersion"
+	$version = [Version]::new($targetVersion)
+	Set-CarpenterVariable -VariableName "Carpenter.Version.Major" -Value $version.Major
+	Set-CarpenterVariable -VariableName "Carpenter.Version.Minor" -Value $version.Minor
+	Set-CarpenterVariable -VariableName "Carpenter.Version.Patch" -Value $version.Patch
+}
