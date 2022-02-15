932ddc99657304feed1a5e4bdc768daaf3d16309
Jeffrey Block <jeffrey.block@suent.net>
2022-02-15 10:41:42 -0700

    Add Carpenter.AzurePipelines project AB#120 (#20)
    * Add NuGet.Config AB#121
    
    * Add AzurePipelines project AB#121
    
    * Rename development.yml to dev.yml
    
    * Update displayname
    
    * change casing on artifact name
    
    * Update build job display name
    
    * Rename Dev to Developer
    
    * Add Code of Conduct
    
    * Add contributing guidelines
    
    * Rename documents folder to docs
    
    * Update links to docs
    
    * Remove tests that require pipeline run
    
    * Build and publish binaries and nuget artifacts
    
    * Pass buildDotNet parameter
    
    * Fix assembly version
    
    * Remove warning for semver2 package identifier
    
    * Clean nuget folder

--- README.md
+++ README.md
@@ -5,15 +5,21 @@ The carpenter-azure-pipelines project provides common YAML templates and scripts
 
 ## Features
 
-* [Build Versioning](doc/build-versioning.md)
-  * [Build Revision](doc/build-revision.md)
-  * [Semantic Versioning 2.0.0](doc/semver.md)
-* [Configurable Pool](doc/configure-pool.md)
-* [Pipeline Versioning](doc/pipeline-versioning.md)
-* [Carpenter Variables](doc/variables.md)
+* [Build Versioning](docs/build-versioning.md)
+  * [Build Revision](docs/build-revision.md)
+  * [Semantic Versioning 2.0.0](docs/semver.md)
+* [Configurable Pool](docs/configure-pool.md)
+* [Pipeline Versioning](docs/pipeline-versioning.md)
+* [Carpenter Variables](docs/variables.md)
 
 ## Usage
 
 | Method | Description |
 |:-------|:------------|
-| [Direct Access](doc/usage-direct.md) | Directly access carpenter-azure-pipelines by referencing from your projects pipeline yaml. |
+| [Direct Access](docs/usage-direct.md) | Directly access carpenter-azure-pipelines by referencing from your projects pipeline yaml. |
+
+# Development
+
+For details on development, please see the [Carpenter development wiki](https://dev.azure.com/suent/Carpenter/_wiki/wikis/Carpenter.wiki).
+
+Please also take note of the carpenter-azure-pipelines [Code of Conduct](CODE_OF_CONDUCT.md) and [Contributing guidelines](CONTRIBUTING.md).
---
+++ src/AzurePipelines/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+    Carpenter
+    A cross-platform dotnet (.NET) build process helper.
+
+
+    Copyright © 2015-2019 Suent Networks, All rights reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+// SPDX-License-Identifier: MIT
+
+namespace Suent.Carpenter.AzurePipelines
+{
+    /// <summary>
+    /// Used for code coverage metric generation.
+    /// </summary>
+    /// <remarks>
+    /// Can be removed if/when this assembly contains actual functionality and is not just used for versionining.
+    /// </remarks>
+    public static class Program
+    {
+        /// <summary>
+        /// Has this class been loaded?
+        /// </summary>
+        public static readonly bool IsLoaded = true;
+
+    } // Program class
+
+} // Suent.Carpenter.AzurePipelines namespace
