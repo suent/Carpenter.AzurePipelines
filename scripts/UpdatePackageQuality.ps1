2e63915ba7407e1da904f16d0b928c9e912881b2
Jeffrey Block <jeffrey.block@suent.net>
2022-02-25 11:20:41 -0700

    Fix uri to nuget feed

--- scripts/UpdatePackageQuality.ps1
+++ scripts/UpdatePackageQuality.ps1
@@ -2,7 +2,8 @@
 param(
 	[Parameter(Mandatory=$True)]
 	[string] $PackagePath,
-	[string] $CollectionUri = $env:SYSTEM_COLLECTIONURI,
+	[string] $ArtifactFeed = "https://pkgs.dev.azure.com",
+	[string] $Organization = $env:SYSTEM_COLLECTIONURI,
 	[string] $TeamProject = $env:SYSTEM_TEAMPROJECT,
 	[Parameter(Mandatory=$True)]
 	[string] $FeedName,
@@ -22,7 +23,7 @@ try {
 	Get-ChildItem . -Filter *.nupkg | Foreach-Object {
 	  $matches = $nameExpression.Match($_.Name)
 	  $packageName = $matches.groups['name']
-	  $requestUri = $CollectionUri + $teamProject + "/_apis/packaging/feeds/$feedName/nuget/packages/$packageName/versions/$packageVersion" + "?api-version=5.0-preview.1"
+	  $requestUri = $ArtifactFeed + $Organization + $TeamProject + "/_apis/packaging/feeds/$feedName/nuget/packages/$packageName/versions/$packageVersion" + "?api-version=7.1-preview.1"
 	  Write-Verbose -Message $requestUri
 	  $head = @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }
 	  $reponse = Invoke-RestMethod -Uri $requestUri -Headers $head -ContentType "application/json" -Method Patch -Body $json
